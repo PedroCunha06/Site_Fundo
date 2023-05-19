7388a33fba63732d4d4ef65a14b6f97eacdbecdb
Pedro Cunha <pdh.alves@outlook.com>
2023-05-19 15:27:31 -0300

    Update 003

--- javascript/java.js
+++ javascript/java.js
@@ -4,6 +4,9 @@ Declaração de Variáveis
 let botao = document.querySelector('#botao');
 let escolha_fundo;
 let escolha_cor;
+let escolha_rotate;
+let escolha_animation_fundo;
+let escolha_stars;
 let aplicar = document.querySelector('#aply');
 /* 
 Eventos
@@ -23,51 +26,110 @@ function settings() {
     let div = document.querySelector('#config');
     let color = document.querySelector('.color');
     let face = document.querySelectorAll('.face');
+    let cube = document.querySelector('#cube_animation');
+    let stars = document.querySelector('.stars');
 
     switch (escolha_fundo) {
         case "dark1": {
             color.style.background = "linear-gradient(45deg, var(--dark1))";
+            color.style.backgroundSize = "300% 300%";
             break;
         }
         case "dark2": {
             color.style.background = "linear-gradient(45deg, var(--dark2))";
+            color.style.backgroundSize = "300% 300%";
             break
         }
         case "night": {
             color.style.background = "linear-gradient(45deg, var(--night))";
+            color.style.backgroundSize = "300% 300%";
             break;
         }
         case "day1": {
             color.style.background = "linear-gradient(45deg, var(--day1))";
+            color.style.backgroundSize = "300% 300%";
             break;
         }
         case "day2": {
             color.style.background = "linear-gradient(45deg, var(--day2))";
+            color.style.backgroundSize = "300% 300%";
             break;
         }
         case "day3": {
             color.style.background = "linear-gradient(45deg, var(--day3))";
+            color.style.backgroundSize = "300% 300%";
             break;
         }
     }
+
+    switch (escolha_stars) {
+        case "yes": {
+            stars.style.display = "block";
+            break;
+        }
+        case "no": {
+            stars.style.display = "none";
+            break;
+        }
+    }
+
+    switch (escolha_animation_fundo) {
+        case "yes": {
+            color.style.animation = "colors 15s ease infinite";
+            break;
+        }
+        case "no": {
+            color.style.animation = "none";
+            break;
+        }
+    }
+
     switch (escolha_cor) {
         case "blue": {
-            face.style.background = "linear-gradient(-180deg, var(--darkblue))";
+            for (let i = 0; i < face.length; i++) {
+                face[i].style.background = "linear-gradient(-180deg, var(--darkblue))";
+                face[i].style.backgroundSize = "175% 175%";
+            }
             break;
         }
         case "red": {
-            face.style.background = "linear-gradient(-180deg, var(--darkred))";
+            for (let i = 0; i < face.length; i++) {
+                face[i].style.background = "linear-gradient(-180deg, var(--darkred))";
+                face[i].style.backgroundSize = "175% 175%";
+            }
             break;
         }
         case "yellow": {
-            face.style.background = "linear-gradient(-180deg, var(--darkyellow))";
+            for (let i = 0; i < face.length; i++) {
+                face[i].style.background = "linear-gradient(-180deg, var(--darkyellow))";
+                face[i].style.backgroundSize = "175% 175%";
+            }
             break;
         }
         case "pink": {
-            face.style.background = "linear-gradient(-180deg, var(--darkpink))";
+            for (let i = 0; i < face.length; i++) {
+                face[i].style.background = "linear-gradient(-180deg, var(--darkpink))";
+                face[i].style.backgroundSize = "175% 175%";
+            }
             break;
         }
     }
+
+    switch (escolha_rotate) {
+        case "rota0": {
+            cube.style.animation = "none";
+            break;
+        }
+        case "rota1": {
+            cube.style.animation = "rotate1 10s linear infinite"
+            break;
+        }
+        case "rota2": {
+            cube.style.animation = "rotate2 10s linear infinite"
+            break;
+        }
+    }
+
     div.style.display = "none";
 }
 
@@ -78,4 +140,16 @@ function escolhaFundo(id) {
 
 function escolhaCor(id) {
     escolha_cor = id;
+}
+
+function escolhaRota(id) {
+    escolha_rotate = id;
+}
+
+function escolhaAnimation(id) {
+    escolha_animation_fundo = id;
+}
+
+function escolhaStars(id) {
+    escolha_stars = id;
 }
