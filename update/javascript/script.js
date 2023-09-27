d980a679eed89a4fcf5172f000c1b7e3540a5c3b
Pedro Cunha <pdh.alves@outlook.com>
2023-09-27 13:22:13 -0300

    Update 024 - Finish first animation face

--- update/javascript/script.js
+++ update/javascript/script.js
@@ -1,19 +1,22 @@
 const FACE_CUBE = 275;
 const SPACE_CUBE = (FACE_CUBE / 2);
 const BACKGROUNDSIZE = 100;
+const BACKGROUND = 300;
 
-const  backElem = document.querySelector('[data-background]')
+const backElem = document.querySelector('[data-background]')
 
 const cubeElem = document.querySelector('[data-cube]');
 const faceElem = document.querySelectorAll('[data-face]');
-const faceUpElem =document.querySelector('[data-faceUp]');
-const faceDownElem =document.querySelector('[data-faceDown]');
-const faceShadowElem =document.querySelector('[data-faceShadow]');
+const faceUpElem = document.querySelector('[data-faceUp]');
+const faceDownElem = document.querySelector('[data-faceDown]');
+const faceShadowElem = document.querySelector('[data-faceShadow]');
 
 const buttonElem = document.querySelector('[data-button]');
 const buttonBackElem = document.querySelector('[data-back]');
 const buttonApplyElem = document.querySelector('[data-apply]');
-const boxConfigElem = document.querySelector('[data-box]')
+const boxConfigElem = document.querySelector('[data-box]');
+
+let escolha;
 
 setPixelToWorldScale()
 window.addEventListener('resize', setPixelToWorldScale);
@@ -32,7 +35,7 @@ function setPixelToWorldScale() {
     if (worldToPixelScale > 1.5) {
         worldToPixelScale = 1.5;
     }
-   // Calcular uma formula q em certo estagio da tela, a proporcao nao cresca, mas diminua
+    // Calcular uma formula q em certo estagio da tela, a proporcao nao cresca, mas diminua
     console.log(window.innerHeight)
     console.log(window.innerWidth)
     console.log(worldToPixelScale)
@@ -51,9 +54,10 @@ function setFaceToWorldScale(worldToPixelScale) {
 }
 
 function openBoxConfig() {
-    if(boxConfigElem.style.display != "flex") {
+    if (boxConfigElem.style.display != "flex") {
         boxConfigElem.style.display = "flex";
         buttonElem.style.display = "none"
+        resetLoadFace();
     } else {
         boxConfigElem.style.display = "none";
         buttonElem.style.display = "block"
@@ -67,22 +71,35 @@ function ApplyCube() {
     const faceDown = getValueFaceDown();
     const border = getValuesBorder();
 
-    faceElem.forEach((face) => {
-        face.style.background = faces;
-        face.style.backgroundSize = `${BACKGROUNDSIZE}%, ${BACKGROUNDSIZE}%`
-        face.style.border = border;
-    });
+    if (escolha !== faces) {
+        setLoadFace()
+    }
+    setTimeout(() => {
+        // FACES CHANGES
+        faceElem.forEach((face) => {
+            face.style.background = faces;
+            face.style.backgroundSize = `${BACKGROUNDSIZE}%, ${BACKGROUNDSIZE}%`
+            face.style.border = border;
+        });
         faceUpElem.style.backgroundColor = faceUp.value;
         faceDownElem.style.backgroundColor = faceDown.value;
         faceShadowElem.style.backgroundColor = faceDown.value;
+        faceShadowElem.style.display = "block";
         faceShadowElem.style.boxShadow = `0px 20px 200px 150px ${faceDown.value}`
-       
-    
 
-    backElem.style.background = background;
 
-    
+        // BACK CHANGES
+        backElem.style.background = background;
+        backElem.style.backgroundSize = `${BACKGROUND}%, ${BACKGROUND}%`
+        backElem.style.animation = getAnimationBackGround();
+        console.log(getAnimationBackGround())
+
+
+    }, 1000);
+
+
     openBoxConfig();
+    escolha = getValuesFace();
 }
 
 function getValuesBackGround() {
@@ -91,10 +108,16 @@ function getValuesBackGround() {
     let x = document.querySelector("#back2");
     let z = document.querySelector("#back3");
     escolha = `linear-gradient(90deg,  ${y.value}, ${x.value}, ${z.value})`
-    
+
     return (
-       escolha
+        escolha
     )
+}
+
+function getAnimationBackGround() {
+    const no = document.querySelector('#backNo');
+
+    return no.checked ? `none` : `animationBackGround 5s ease infinite reverse`;
 }
 
 function getValuesFace() {
@@ -112,8 +135,9 @@ function getValueFaceTop() {
     let x = document.querySelector("#color1");
     return x;
 }
+
 function getValueFaceDown() {
-     let x = document.querySelector("#color2");
+    let x = document.querySelector("#color2");
     return x;
 }
 
@@ -125,4 +149,19 @@ function getValuesBorder() {
     console.log(no.click)
 
     return no.checked ? `none` : `1px solid ${x.value}`;
+}
+
+function setLoadFace() {
+    faceElem.forEach((face) => {
+        face.setAttribute("id", "changeColorFace")
+    });
+    faceUpElem.style.backgroundColor = "white";
+    faceDownElem.style.backgroundColor = "white"
+    faceShadowElem.style.display = "none";
+}
+
+function resetLoadFace() {
+    faceElem.forEach((face) => {
+        face.removeAttribute("id", "changeColorFace")
+    })
 }
